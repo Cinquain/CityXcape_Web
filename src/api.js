12b6691bf24a2dec5c1e0cd5697ddabe21b8f5b2
Cinquain <james@cityxcape.com>
2020-06-16 17:48:41 -0400

    changed lambda function

--- src/api.js
+++ src/api.js
@@ -1,42 +1,9 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
-const app = express();
-const router = express.Router()
+module.exports.handler = function(event, context, callback) {
+    
 
-require('dotenv').config();
+    const {first_name, last_name, email, city, device} = event.body
 
 
-app.use(bodyParser.urlencoded({extended:false}));
-
-
-
-router.get('/', (req, res) => {
-    res.json({
-        'Hi': 'Hello World'
-    });
-})
-
-
-router.post('/signup', (req, res, err) =>{
-
-   
-    var first_name = req.body.fname
-    var last_name = req.body.lname
-    var email = req.body.email
-    var city = req.body.city 
-    var device = req.body.device
-    console.log(first_name, last_name, email, city, device)
-    saveToMailchimp(first_name, last_name, email, city, device)
-    res.redirect('https://romantic-rosalind-bb4e9e.netlify.app/success.html')
-
-    if (err) {
-        console.log('error saving user')
-    }
-})
-
-
-function saveToMailchimp(fname, lname, email, city, device) {
 
 
     var request = require("request")
@@ -55,8 +22,8 @@ function saveToMailchimp(fname, lname, email, city, device) {
     { email_address: email,
         status: 'subscribed',
         merge_fields: 
-        { FNAME: fname,
-            LNAME: lname,
+        { FNAME: first_name,
+            LNAME: last_name,
             TEXTYUI_3: city,
             CHECKBOXY: device } },
     json: true };
@@ -67,12 +34,92 @@ function saveToMailchimp(fname, lname, email, city, device) {
     console.log(body);
     });
 
+
+    callback(null, {
+        statusCode: 200,
+        body: 'Hello World
+    }
+    response.redirect('https://romantic-rosalind-bb4e9e.netlify.app/success.html')
+    )
+
 }
 
 
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const router = express.Router()
 
-app.use('/.netlify/functions/api',router);
+// require('dotenv').config();
+
+
+// app.use(bodyParser.urlencoded({extended:false}));
 
 
 
-module.exports.handler = serverless(app)
+// router.get('/', (req, res) => {
+
+//     res.json({
+//         'Hi': 'Hello World'
+//     });
+// })
+
+
+// router.post('/signup', (req, res, err) =>{
+
+//     // var first_name = req.body.fname
+//     // var last_name = req.body.lname
+//     // var email = req.body.email
+//     // var city = req.body.city 
+//     // var device = req.body.device
+//     console.log(first_name, last_name, email, city, device)
+//     saveToMailchimp(first_name, last_name, email, city, device)
+//     res.redirect('https://romantic-rosalind-bb4e9e.netlify.app/success.html')
+
+//     if (err) {
+//         console.log('error saving user')
+//     }
+// })
+
+
+// function saveToMailchimp(fname, lname, email, city, device) {
+
+
+//     var request = require("request")
+
+//     var options = { method: 'POST',
+//     url: 'https://us7.api.mailchimp.com/3.0/lists/cc06da0dc6/members',
+//     headers: 
+//     { 'cache-control': 'no-cache',
+//         Connection: 'keep-alive',
+//         Host: 'us7.api.mailchimp.com',
+//         'Cache-Control': 'no-cache',
+//         Accept: '*/*',
+//         Authorization: process.env.MAILCHIMP_AUTHORIZATION,
+//         'Content-Type': 'application/json' },
+//     body: 
+//     { email_address: email,
+//         status: 'subscribed',
+//         merge_fields: 
+//         { FNAME: fname,
+//             LNAME: lname,
+//             TEXTYUI_3: city,
+//             CHECKBOXY: device } },
+//     json: true };
+
+//     request(options, function (error, response, body) {
+
+//     if (error) throw new Error(error);
+//     console.log(body);
+//     });
+
+// }
+
+
+
+// app.use('/.netlify/functions/api',router);
+
+
+
+// module.exports.handler = serverless(app)
