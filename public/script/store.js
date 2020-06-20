9d8c1b8f899d97860ebb45fc35a66e34a4ed6c33
Cinquain <james@cityxcape.com>
2020-06-20 19:04:40 -0400

    started building ecommerce section

---
+++ public/script/store.js
@@ -0,0 +1,11 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+
+    console.log('hello James Allan')
+
+}
